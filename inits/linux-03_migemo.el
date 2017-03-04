1b9e2a64dfbc63b04fe479213b1021b757aea1d9
yoko <jr4qpv@yahoo.co.jp>
2017-03-04 16:48:17 +0900

    Linux用のmigemo設定を追加

---
+++ inits/linux-03_migemo.el
@@ -0,0 +1,17 @@
+;;
+;; migemo.elの設定 (for Linux)
+;;
+(when (and (executable-find "cmigemo")
+           (require 'migemo nil t))
+  (setq migemo-command "cmigemo")
+  (setq migemo-options '("-q" "--emacs"))
+
+  (setq migemo-dictionary "/usr/share/migemo/utf-8/migemo-dict")
+
+  (setq migemo-user-dictionary nil)
+  (setq migemo-regex-dictionary nil)
+  (setq migemo-coding-system 'utf-8-unix)
+  (load-library "migemo")
+  (migemo-init)
+)
+
