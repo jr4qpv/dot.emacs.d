ef8fed23e17fc8d40c3589b80f57b65e9169f9c7
yoko <jr4qpv@yahoo.co.jp>
2017-03-04 16:54:06 +0900

    Linux用cmigemoの辞書パス誤記訂正

--- inits/linux-03_migemo.el
+++ inits/linux-03_migemo.el
@@ -6,7 +6,7 @@
   (setq migemo-command "cmigemo")
   (setq migemo-options '("-q" "--emacs"))
 
-  (setq migemo-dictionary "/usr/share/migemo/utf-8/migemo-dict")
+  (setq migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict")
 
   (setq migemo-user-dictionary nil)
   (setq migemo-regex-dictionary nil)
