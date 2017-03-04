c3ed0b00597f6ce484109a651960a3361423db57
yoko <jr4qpv@yahoo.co.jp>
2017-03-04 16:37:13 +0900

    macOS専用のmigemo設定ファイルにした

--- inits/cocoa-emacs-03_migemo.el
+++ inits/cocoa-emacs-03_migemo.el
@@ -1,2 +1,17 @@
-(setq migemo-command "/usr/local/bin/cmigemo")
-(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+;;
+;; migemo.elの設定 (for macOS)
+;;
+(when (and (executable-find "cmigemo")
+           (require 'migemo nil t))
+  (setq migemo-command "cmigemo")
+  (setq migemo-options '("-q" "--emacs"))
+
+  (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+
+  (setq migemo-user-dictionary nil)
+  (setq migemo-regex-dictionary nil)
+  (setq migemo-coding-system 'utf-8-unix)
+  (load-library "migemo")
+  (migemo-init)
+)
+
