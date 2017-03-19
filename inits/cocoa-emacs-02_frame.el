832320a844494f086845ec7736e6f249e7c4d5b9
yoko <jr4qpv@yahoo.co.jp>
2017-03-19 14:27:15 +0900

    macOSでスクリーンの縦が800ドットの時はフォントサイズ12にする

--- inits/cocoa-emacs-02_frame.el
+++ inits/cocoa-emacs-02_frame.el
@@ -12,7 +12,11 @@
 ;; モニタ解像度に応じてフォントサイズを変える
 ;; See: http://qiita.com/syohex/items/7b8dccd8682d7db9cf5c
 ;; ※高さが1100以上の場合はフォントサイズを18,それ未満は16にする
-(let ((size (if (>= (x-display-pixel-height) 1100) 18 16)))
+(let ((size (cond
+	     ((>= (x-display-pixel-height) 1100) 18)
+	     ((>= (x-display-pixel-height) 900) 16)
+	     (t 12) )
+       ))
   (condition-case err
       (let ((myfont (format "Osaka-%d" size)))
     (set-frame-font myfont)
