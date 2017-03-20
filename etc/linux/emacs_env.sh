11bad25b1344c51e28df30ee656b8833605c90e6
yoko <jr4qpv@yahoo.co.jp>
2017-03-20 10:03:18 +0900

    linuxの起動常駐時のメッセージは画面に表示せずにログファイルに残す

--- etc/linux/emacs_env.sh
+++ etc/linux/emacs_env.sh
@@ -2,7 +2,8 @@
 if emacsclient -e "t" > /dev/null 2>&1 ; then
     echo 'Emacs daemon is already running.'
 else
-    emacs --daemon
+    echo 'Starting Emacs daemon.'
+    emacs --daemon > ~/.emacs.d/etc/log/emacs_start.log 2>&1
 fi
 
 export EDITOR='emacsclient -a "" -t'
