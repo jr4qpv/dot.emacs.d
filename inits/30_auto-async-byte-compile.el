bb1d98f867b5a7ad9608e79e8950d5ef75b13582
yoko <jr4qpv@yahoo.co.jp>
2017-06-22 11:23:07 +0900

    初期設定フォルダ inits/ を全てバイトコンパイルし、auto-async-byte-compileを有効にした

--- inits/30_auto-async-byte-compile.el
+++ inits/30_auto-async-byte-compile.el
@@ -1,6 +1,4 @@
 ;;; auto-async-byte-compileの設定
 (require 'auto-async-byte-compile)
 (setq auto-async-byte-compile-exclude-files-regexp "/junk/")
-(setq auto-async-byte-compile-exclude-files-regexp "/inits/")
 (add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
-
