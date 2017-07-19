ec125095dc4b36493526139b6f1dfb91f48a1598
yoko <jr4qpv@yahoo.co.jp>
2017-07-19 11:20:46 +0900

    diredで同じバッファーでディレクトリを開くように設定

--- inits/50_dired.el
+++ inits/50_dired.el
@@ -2,50 +2,24 @@
 ;; diredの設定
 ;;
 
-;; 2画面なら他方にコピー
-(setq dired-dwim-target t)
+;; ファイルなら別バッファで、ディレクトリなら同じバッファで開く
+(defun dired-open-in-accordance-with-situation ()
+    (interactive)
+    (cond ((string-match "\\(?:\\.\\.?\\)"
+                         (format "%s" (thing-at-point 'filename)))
+           (dired-find-alternate-file))
+          ((file-directory-p (dired-get-filename))
+           (dired-find-alternate-file))
+          (t
+           (dired-find-file))))
 
-(defun dired-dwim-find-alternate-file ()
-  "画面分割に適した `dired-find-alternate-file'．
-通常は `dired-find-alternate-file' を行うが，画面分割されていて
-他方のウィンドウに同じバッファが表示されていれば `dired-find-file'．"
-  (interactive)
-  (cond
-   ;; 同じバッファが他のwindowにある場合
-   ((delq (selected-window) (get-buffer-window-list))
-    (dired-find-file))
-   ;; 同じバッファが他のwindowにない場合
-   (t
-    (dired-find-alternate-file))))
+;; dired-find-alternate-file の有効化
+(put 'dired-find-alternate-file 'disabled nil)
+;; RET 標準の dired-find-file では dired バッファが複数作られるので
+;; dired-find-alternate-file を代わりに使う
+(define-key dired-mode-map (kbd "RET") 'dired-open-in-accordance-with-situation)
+(define-key dired-mode-map (kbd "a") 'dired-find-file)
 
-
-(defun dired-up-alternate-directory ()
-  "バッファを増やさず上のディレクトリに移動．"
-  (interactive)
-  (let* ((dir (dired-current-directory))
-         (up (file-name-directory (directory-file-name dir))))
-    (or (dired-goto-file (directory-file-name dir))
-        ;; Only try dired-goto-subdir if buffer has more than one dir.
-        (and (cdr dired-subdir-alist)
-             (dired-goto-subdir up))
-        (progn
-          (find-alternate-file up)
-          (dired-goto-file dir)))))
-
-
-(defun dired-dwim-up-alternate-directory ()
-  "画面分割に適した `dired-up-alternate-directory'．"
-  (interactive)
-  (cond
-   ;; 同じバッファが他のwindowにある場合
-   ((delq (selected-window) (get-buffer-window-list))
-    (dired-up-directory))
-   ;; 同じバッファが他のwindowにない場合
-   (t
-    (dired-up-alternate-directory))))
-
-
-(defun dired-dwim-quit-window ()
-  "画面分割に適した `quit-window'．"
-  (interactive)
-  (quit-window (not (delq (selected-window) (get-buffer-window-list)))))
+;; ディレクトリの移動キーを追加(wdired 中は無効)
+(define-key dired-mode-map (kbd "<left>") 'dired-up-directory)
+(define-key dired-mode-map (kbd "<right>") 'dired-open-in-accordance-with-situation)
