f62770decd6b277ead5fee73beebae044620008d
yoko <jr4qpv@yahoo.co.jp>
2018-03-17 22:22:42 +0900

    X-Windows時は最小化しない記述に変更

--- inits/02_server.el
+++ inits/02_server.el
@@ -13,17 +13,13 @@
     (global-set-key (kbd "C-x C-c") 'server-edit)
     ;; M-x exitでEmacsを終了できるようにする
     (defalias 'exit 'save-buffers-kill-emacs)
+    
+    ;; 起動時に最小化する
+    (if (not (eq window-system 'x) )
+	(add-hook 'after-init-hook 'iconify-emacs-when-server-is-done) )
 
+    ;; 終了時にyes/noの問い合わせ
     (if (eq window-system 'w32)
-	;; 起動時に最小化する
-	(add-hook 'after-init-hook 'iconify-emacs-when-server-is-done)
-      
-	;; 終了時にyes/noの問い合わせ
 	(setq confirm-kill-emacs 'yes-or-no-p) ) 
     )
-  
-    (if (eq window-system 'mac)
-	;; 起動時に最小化する
-	(add-hook 'after-init-hook 'iconify-emacs-when-server-is-done)
-      ) 
   )
