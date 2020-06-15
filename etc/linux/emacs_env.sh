1d0df921a3b8b4745d78ddc789f246ab858c2b76
yoko <jr4qpv@yahoo.co.jp>
2020-06-15 15:57:58 +0900

    Ubuntuの初期スクリプトでは、DISPLAY環境変数判定を追加

--- etc/linux/emacs_env.sh
+++ etc/linux/emacs_env.sh
@@ -1,11 +1,17 @@
 # emacs_env.sh for Ubuntu
-if emacsclient -e "t" > /dev/null 2>&1 ; then
+
+if [ -v DISPLAY ]; then
+  export EDITOR='emacsclient -n'
+  alias e=${EDITOR}
+else    
+  if emacsclient -e "t" > /dev/null 2>&1 ; then
     echo 'Emacs daemon is already running.'
-else
+  else
     echo 'Starting Emacs daemon.'
     emacs --daemon > ~/.emacs.d/etc/log/emacs_start.log 2>&1
-fi
+  fi
 
-export EDITOR='emacsclient -a "" -t'
-alias e=${EDITOR}
-alias ekill='emacsclient -e "(kill-emacs)"'
+  export EDITOR='emacsclient -a "" -t'
+  alias e=${EDITOR}
+  alias ekill='emacsclient -e "(kill-emacs)"'
+fi
--- etc/linux/emacs_env_dt.sh
+++
@@ -1,3 +0,0 @@
-# emacs_env_dt.sh for Ubuntu Desktop
-export EDITOR='emacsclient -n'
-alias e=${EDITOR}
