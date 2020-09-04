ff9f7e0935c8a754990bea223de29a2af72ce1e3
yoko <jr4qpv@yahoo.co.jp>
2020-09-04 09:43:52 +0900

    macOSでのEmacsテキスト起動のalias定義追加

--- etc/macos/emacs_env.sh
+++ etc/macos/emacs_env.sh
@@ -5,5 +5,5 @@ alias e=${EDITOR}
 ##alias e='emacsclient -a "" -t'
 alias ec='emacsclient'
 alias ee='open -a /Applications/Emacs.app $1'
-alias em='emacs'
+alias em='emacs -nw'
 alias ekill='emacsclient -e "(kill-emacs)"'
