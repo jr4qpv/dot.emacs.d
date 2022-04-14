20b05e70c94a67e407a627e3e88dc18bb47563da
yoko <jr4qpv@yahoo.co.jp>
2022-04-14 16:28:44 +0900

    neotreeパッケージをインストールし有効にした

---
+++ inits/50_neotree.el
@@ -0,0 +1,10 @@
+;;
+;; neotreeの設定
+;;
+(require 'neotree)
+
+;;----- キーに割り当てる
+(global-set-key (kbd "<f9>") 'neotree-toggle)
+(global-set-key (kbd "C-c n") 'neotree-toggle)
+;;(global-set-key (kbd "C-c C-n") 'neotree-toggle)
+
