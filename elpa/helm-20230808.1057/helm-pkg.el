6447c4e9754a3809415393bb88600ad0ed328dbb
yoko <jr4qpv@yahoo.co.jp>
2023-08-10 11:32:50 +0900

    helm, tabbar パッケージの更新

---
+++ elpa/helm-20230808.1057/helm-pkg.el
@@ -0,0 +1,14 @@
+(define-package "helm" "20230808.1057" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.9.3")
+    (wfnames "1.1")
+    (popup "0.5.3"))
+  :commit "a968b9bdd243f9d8d9d572656268421cb1ea8b39" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/helm-core-20230808.1057/helm-core-pkg.el
@@ -0,0 +1,13 @@
+(define-package "helm-core" "20230808.1057" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.7"))
+  :commit "a968b9bdd243f9d8d9d572656268421cb1ea8b39" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
